d69eac31a0e2928d4cdbc7304b3724fc1e196c07
CtrlAltRee <dess.rucker@gmail.com>
2022-03-23 01:17:58 -0400

    more changes

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function hoverFunction2() {
 
 myImage.addEventListener("mouseenter", hoverFunction);
 myImage.addEventListener("mouseleave", hoverFunction2);
+
+// let myImage1 = document.getElementById("imgArt1");
+
+// function hoverFunction() {
+//     myImage1.style.transform = "scale(1.2, 1.2)";
+// }
+
+// function hoverFunction2() {
+//     myImage1.style.transform = "scale(1,1)";
+// }
+
+// myImage1.addEventListener("mouseenter", hoverFunction);
+// myImage1.addEventListener("mouseleave", hoverFunction2);
