55cdb0d1e326886afd36a37b23633b9619e75452
CtrlAltRee <dess.rucker@gmail.com>
2022-03-24 01:18:04 -0400

    style cleanups

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-let myImage = document.getElementById("imgArt");
+// let myImage = document.getElementsByClassName("imgArt");
+//     console.log(myImage)
+// for (let i = 0; i < myImage.length; i++) {
+//     myImage[i].addEventListener("mouseenter", alert("hover on image"));
+//     myImage[i].addEventListener("mouseleave", alert("hover out image"));
+//     }
 
-function hoverFunction() {
-    myImage.style.transform = "scale(1.2, 1.2)";
-}
+let myImage = document.querySelectorAll(".imgArt");
 
-function hoverFunction2() {
-    myImage.style.transform = "scale(1,1)";
-}
+myImage.forEach(function(element) {
+    element.addEventListener("mouseenter", function() {
+        this.classList.add("artwork");
+    });
+    element.addEventListener("mouseleave", function() {
+        this.classList.remove("artwork");
+    });
+});
 
-myImage.addEventListener("mouseenter", hoverFunction);
-myImage.addEventListener("mouseleave", hoverFunction2);
+// let myImage = document.getElementsByClassName('.imgArt');
+
+// myImage = Array.prototype.slice.call(myImage);
+
+// myImage.forEach( function(element){
+//     element.style.transform = "scale(1.2, 1.2)"; 
+// });
+
+// function hoverFunction() {
+//    myImage.style.transform = "scale(1.2, 1.2)";
+// }
+
+// function hoverFunction2() {
+//     myImage.style.transform = "scale(1,1)";
+// }
+
+// myImage.addEventListener("mouseenter", hoverFunction);
+// myImage.addEventListener("mouseleave", hoverFunction2);
 
 // let myImage1 = document.getElementById("imgArt1");
 
